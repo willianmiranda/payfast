2d817c481cfbc17ee10ee376ff77ee40a47119eb
willianminrada <willian_wbm@hotmail.com>
2017-08-19 23:04:56 -0300

    atualizando

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -4,50 +4,8 @@ module.exports = (app) => {
         res.send('OK');
     });
 
-    app.delete('/pagamentos/pagamento/:id', (req, res) => {
-
-        let id = req.params.id;
-
-        let pagamento = {};
-        pagamento.id = id;
-        pagamento.status = "cancelado";
-
-        let connection = app.persistencia.connectionFactory();
-        let pagamentoDAO = app.persistencia.PagamentoDAO(connection);
-
-        pagamentoDAO.atualiza(pagamento, (erro) => {
-            if (erro) {
-                res.status(500).send(erro);
-                return;
-            }
-            console.log('pagamento cancelado');
-            res.send(pagamento);
-        });
-    });
-
-    app.put('/pagamentos/pagamento/:id', (req, res) => {
-
-        let id = req.params.id;
-
-        let pagamento = {};
-        pagamento.id = id;
-        pagamento.status = "confirmado";
-
-        let connection = app.persistencia.connectionFactory();
-        let pagamentoDAO = app.persistencia.PagamentoDAO(connection);
-
-        pagamentoDAO.atualiza(pagamento, (erro) => {
-            if (erro) {
-                res.status(500).send(erro);
-                return;
-            }
-            console.log('pagamento confirmado');
-            res.send(pagamento);
-        });
-    });
-
     app.post('/pagamentos/pagamento', (req, res) => {
-
+        
         req
             .assert("forma_de_pagamento", "forma de pagamento é obrigatório")
             .notEmpty();
@@ -65,9 +23,9 @@ module.exports = (app) => {
         }
 
         let pagamento = req.body;
-
+        
         console.log(pagamento);
-
+        
         pagamento.status = 'criado';
         pagamento.data = new Date();
 
@@ -80,7 +38,7 @@ module.exports = (app) => {
                 res.status(500).send(erro);
             } else {
                 console.log('pagamento salvo');
-                res.location('/pagamentos/pagamento/' + resultado.insertId);
+                res.location('/pagamentos/pagamento/' + resultado.insertIde);
                 res.status(201).json(pagamento);
             }
         });
