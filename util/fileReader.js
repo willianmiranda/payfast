bded76153f643dfe65fc1fbe5a4d8ad93f4f2df9
Willian Miranda <willianmiranda@bitbucket.org>
2017-09-12 19:59:18 -0300

    file reader

---
+++ util/fileReader.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+fs.readFile('imagem.jpg', function (error, buffer) {
+    console.log('arquivo lido');
+
+    fs.writeFile('imagem2.jpg', buffer, function(){
+        console.log('arquivo escrito');
+    });
+})
